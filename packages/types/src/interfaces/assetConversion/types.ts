5d6b0369355308a7e1572b0de8c0db96b6c02168
Chralt98 <chralt98@gmail.com>
2025-01-21 15:55:35 +0100

    use MultiLocationV3 for TAssetConversion ts type

--- packages/types/src/interfaces/assetConversion/types.ts
+++ packages/types/src/interfaces/assetConversion/types.ts
@@ -2,9 +2,9 @@
 /* eslint-disable */
 
 import type { Option } from '@polkadot/types-codec';
-import type { MultiLocation } from '@polkadot/types/interfaces/xcm';
+import type { MultiLocationV3 } from '@polkadot/types/interfaces/xcm';
 
 /** @name TAssetConversion */
-export interface TAssetConversion extends Option<MultiLocation> {}
+export interface TAssetConversion extends Option<MultiLocationV3> {}
 
 export type PHANTOM_ASSETCONVERSION = 'assetConversion';
